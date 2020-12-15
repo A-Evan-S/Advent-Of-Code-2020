7c8ed7b8a495589ad5d81ecefda9ff1456b83f5d
Evan <aevanschor@gmail.com>
2020-12-14 21:24:11 -0800

    Completed Day 15

---
+++ Day 15/day15.py
@@ -0,0 +1,36 @@
+def main():
+    input = []
+    with open('day15_input.txt') as f:
+        for line in f:
+            input.append(line.strip())
+    print("Part 1:", part1(input))
+    print("Part 2:", part2(input))
+
+def find_nth_number(input, n):
+    nums = input[0].split(',')
+    prev_nums = dict()
+    curr_time = 0
+    for num in nums:
+        prev_nums[int(num)] = curr_time
+        curr_time += 1
+    next_number = 0
+    while True:
+        if next_number not in prev_nums.keys():
+            prev_nums[next_number] = curr_time
+            next_number = 0
+        else:
+            old_time = prev_nums[next_number]
+            prev_nums[next_number] = curr_time
+            next_number = curr_time - old_time
+        curr_time += 1
+        if curr_time == n - 1:
+            return next_number
+
+def part1(input):
+    return find_nth_number(input, 2020)
+
+def part2(input):
+    return find_nth_number(input, 30000000)
+
+if __name__ == '__main__':
+    main()
