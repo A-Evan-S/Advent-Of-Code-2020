1e39ef88b8f6af70a0b00e8e8782be878310b0e4
Evan <aevanschor@gmail.com>
2020-12-16 14:45:01 -0800

    removed absolute path

--- run_all_days.py
+++ run_all_days.py
@@ -2,10 +2,11 @@ import importlib
 import os
 
 def main():
+    cwd = os.getcwd()
     for day_num in range(1,26):
         print(f'             --- Day {day_num:2} ---')
         try:
-            os.chdir(f'C:/Users/aevan/PycharmProjects/Advent Of Code 2020/Day_{day_num:02}')
+            os.chdir(f'{cwd}/Day_{day_num:02}')
             module = importlib.import_module(f'Day_{day_num:02}.day{day_num}')
             module.main()
         except:
