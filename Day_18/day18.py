c1dd2df2925fda7c3c366d8da82db6e09057bc9b
Evan <aevanschor@gmail.com>
2020-12-17 22:34:05 -0800

    Simplified day 18

--- Day_18/day18.py
+++ Day_18/day18.py
@@ -24,19 +24,15 @@ def part2(input):
 def evaluate(input):
     if m := re.search(r'\([^\(\)]*?\)', input):
         return evaluate(input[:m.start()] + evaluate(m.group()[1:-1]) + input[m.end():])
-    elif m := re.match('^\d+$', input):
-        return input
     elif m := re.match('^\d+ [\+\*] \d+', input):
         return evaluate(str(eval(m.group())) + input[m.end():])
     else:
-        return 'Error'
+        return input
 
 def evaluate2(input):
     if m := re.search(r'\([^\(\)]*?\)', input):
         input = input[:m.start()] + evaluate2(m.group()[1:-1]) + input[m.end():]
         return evaluate2(input)
-    elif m := re.match('^\d+$', input):
-        return input
     elif m := re.search('\d+ \+ \d+', input):
         a, op, b = m.group().split()
         return evaluate2(input[:m.start()] + str(int(a) + int(b)) + input[m.end():])
@@ -44,7 +40,7 @@ def evaluate2(input):
         a, op, b = m.group().split()
         return evaluate2(input[:m.start()] + str(int(a) * int(b)) + input[m.end():])
     else:
-        return 'Error'
+        return input
 
 if __name__ == '__main__':
     main()
