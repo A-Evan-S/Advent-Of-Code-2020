5890838ea476dea5a22460fd286849f9ad25b227
Evan <aevanschor@gmail.com>
2020-12-17 21:40:34 -0800

    Completed Day 18

--- Day_18/day18.py
+++ Day_18/day18.py
@@ -1,17 +1,50 @@
 from aoc_utils import timed
+import re
 
 def main():
     input = []
-    with open('test_input.txt') as f:
-        input = f.read()
+    with open('day18_input.txt') as f:
+        for line in f:
+            input.append(line.strip())
     print("Part 1:", timed(part1, input))
     print("Part 2:", timed(part2, input))
 
 def part1(input):
-    return 'unfinished'
+    result = 0
+    for line in input:
+        result += int(evaluate(line))
+    return result
 
 def part2(input):
-    return 'unfinished'
+    result = 0
+    for line in input:
+        result += int(evaluate2(line))
+    return result
+
+def evaluate(input):
+    if m := re.search(r'\([^\(\)]*?\)', input):
+        return evaluate(input[:m.start()] + evaluate(m.group()[1:-1]) + input[m.end():])
+    elif m := re.match('^\d+$', input):
+        return input
+    elif m := re.match('^\d+ [\+\*] \d+', input):
+        return evaluate(str(eval(m.group())) + input[m.end():])
+    else:
+        return 'Error'
+
+def evaluate2(input):
+    if m := re.search(r'\([^\(\)]*?\)', input):
+        input = input[:m.start()] + evaluate2(m.group()[1:-1]) + input[m.end():]
+        return evaluate2(input)
+    elif m := re.match('^\d+$', input):
+        return input
+    elif m := re.search('\d+ \+ \d+', input):
+        a, op, b = m.group().split()
+        return evaluate2(input[:m.start()] + str(int(a) + int(b)) + input[m.end():])
+    elif m := re.search('\d+ \* \d+', input):
+        a, op, b = m.group().split()
+        return evaluate2(input[:m.start()] + str(int(a) * int(b)) + input[m.end():])
+    else:
+        return 'Error'
 
 if __name__ == '__main__':
     main()
