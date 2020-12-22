291302d80c210b9bc05207efb28b3e08e9fe599a
Evan <aevanschor@gmail.com>
2020-12-21 16:29:11 -0800

    Updated Day 21 to use sets and intersections for bipartite matching

--- Day_21/day21.py
+++ Day_21/day21.py
@@ -1,5 +1,6 @@
+from collections import defaultdict
 from aoc_utils import timed
-import re, copy
+import re
 
 def main():
     input = []
@@ -11,30 +12,38 @@ def main():
 
 
 def part1(input):
-    recipes, ingredients, allergens = parse_recipes(input)
-    possible_allergens = find_possible_allergens(recipes, ingredients, allergens)
-    safe = list(map(lambda x: x[0], filter(lambda e: e[1] == [], possible_allergens.items())))
+    recipes = parse_recipes(input)
+    possible_allergens = find_possible_allergens(recipes)
+    safe = list(map(lambda x: x[0], filter(lambda e: len(e[1]) == 0, possible_allergens.items())))
     return sum(recipe[0].count(safe_ingredient) for safe_ingredient in safe for recipe in recipes)
 
 
+def part2(input):
+    recipes = parse_recipes(input)
+    possible_allergens = [[k, v] for k, v in find_possible_allergens(recipes).items()]
+    possible_allergens = list(filter(lambda x: len(x[1]) > 0, possible_allergens))
+    for i in range(len(possible_allergens)):
+        for j in range(i + 1, len(possible_allergens)):
+            possible_allergens = sorted(possible_allergens, key=lambda x: len(x[1]))
+            possible_allergens[j][1] -= possible_allergens[j][1].intersection(possible_allergens[i][1])
+    possible_allergens = sorted(possible_allergens, key=lambda x: list(x[1])[0])
+    return ','.join(x[0] for x in possible_allergens)
+
 def parse_recipes(input):
     recipes = []
-    all_ingredients = set()
-    all_allergens = set()
     for line in input:
         ingredient_text, allergen_text = line.split('(contains ')
         ingredients = re.findall('\w+', ingredient_text)
         allergens = list(map(lambda x: x.strip(), allergen_text[:-1].split(', ')))
         recipes.append([ingredients, allergens])
-        all_ingredients.update(ingredients)
-        all_allergens.update(allergens)
-    return recipes, list(all_ingredients), list(all_allergens)
+    return recipes
 
 
-def find_possible_allergens(recipes, ingredients, allergens):
-    ingredients_map = {}
-    for ingredient in ingredients:
-        ingredients_map[ingredient] = copy.deepcopy(allergens)
+def find_possible_allergens(recipes):
+    ingredients_map = defaultdict(lambda: set())
+    for recipe in recipes:
+        for ingredient in recipe[0]:
+            ingredients_map[ingredient].update(recipe[1])
 
     for recipe in recipes:
         for allergen in recipe[1]:
@@ -43,43 +52,6 @@ def find_possible_allergens(recipes, ingredients, allergens):
                     ingredients_map[ingredient].remove(allergen)
 
     return ingredients_map
-
-
-def part2(input):
-    recipes, ingredients, allergens = parse_recipes(input)
-    possible_allergens = find_possible_allergens(recipes, ingredients, allergens)
-    possible_allergens = dict(filter(lambda e: len(e[1]) > 0, possible_allergens.items()))
-    ingredients = list(possible_allergens.keys())
-
-    arr = [[False for _ in range(len(ingredients))] for _ in range(len(allergens))]
-
-    for ing, als in possible_allergens.items():
-        for al in als:
-            arr[allergens.index(al)][ingredients.index(ing)] = True
-
-    old_count = sum([row.count(True) for row in arr])
-    new_count = 0
-
-    while old_count != new_count:
-        old_count = new_count
-        for i in range(len(arr)):
-            if arr[i].count(True) == 1:
-                col_to_clear = arr[i].index(True)
-                for j in range(len(arr)):
-                    if i != j:
-                        arr[j][col_to_clear] = False
-        new_count = sum([row.count(True) for row in arr])
-
-    for i in range(len(ingredients)):
-        allergen = None
-        for j in range(len(arr)):
-            if arr[j][i]:
-                allergen = allergens[j]
-        ingredients[i] = (ingredients[i], allergen)
-
-    ingredients = sorted(ingredients, key=lambda ing: ing[1])
-
-    return ','.join(ing[0] for ing in ingredients)
 
 if __name__ == '__main__':
     main()
