284f83ff1bc4e950f017058c47b225671c82bb18
Evan <aevanschor@gmail.com>
2020-12-24 22:26:34 -0800

    Completed Day 25

--- Day_25/day25.py
+++ Day_25/day25.py
@@ -2,16 +2,22 @@ from aoc_utils import timed
 
 def main():
     input = []
-    with open('test_input.txt') as f:
-        input = f.read()
+    with open('day25_input.txt') as f:
+        for line in f:
+            input.append(int(line.strip()))
     print("Part 1:", timed(part1, input))
-    print("Part 2:", timed(part2, input))
+
 
 def part1(input):
-    return 'unfinished'
-
-def part2(input):
-    return 'unfinished'
+    card_public_key, door_public_key = input
+    subject_number = 7
+    curr_val = 1
+    card_loop_size = 0
+    while curr_val != card_public_key:
+        card_loop_size += 1
+        curr_val *= subject_number
+        curr_val %= 20201227
+    return pow(door_public_key, card_loop_size, 20201227)
 
 if __name__ == '__main__':
     main()
